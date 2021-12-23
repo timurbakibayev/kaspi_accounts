250f9cd22b3ef22e346d1b8d7ec566b92efbd300
Timur Bakibayev <timurbakibayev@gmail.com>
2021-12-23 15:48:13 +0100

    GET and POST

--- webproject/views.py
+++ webproject/views.py
@@ -1,18 +1,26 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
 from account.account import Account
-from database.database import AccountDatabase
 from database.implementations.ram import AccountDatabaseRAM
 import json
 
 database = AccountDatabaseRAM()
-for i in range(3):
-    database.save(Account.random())
+if len(database.get_objects()) == 0:
+    for i in range(3):
+        database.save(Account.random())
 
 
 # Create your views here.
 def accounts(request: HttpRequest) -> HttpResponse:
+
     if request.method == "GET":
         all_accounts = database.get_objects()
         json_view = json.dumps([account.to_json() for account in all_accounts])
         return HttpResponse(content=json_view)
+
+    if request.method == "POST":
+        try:
+            account = Account.from_json_str(request.body.decode("UTF-8"))
+            database.save(account)
+            return HttpResponse(content=account.to_json_str())
+        except Exception as e:
+            return HttpResponse(content=str(e), status=400)
